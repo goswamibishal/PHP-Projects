f25f3e411c77be848216134f3c2d6ed87e9200e8
Bishal Goswami <goswamibishal135@gmail.com>
2018-07-02 11:39:02 +0530

    Update login.php

--- Online Examination Form with login features in Php/login.php
+++ Online Examination Form with login features in Php/login.php
@@ -7,8 +7,6 @@ echo $_SESSION['msg'];
 	<head>
 		<title>login.php</title>
 	</head>
-	
-    
     <body>
     	
     	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
@@ -31,20 +29,6 @@ echo $_SESSION['msg'];
         <button type="submit" class="btn">Login</button>
         </div>
      </form>
-    	
-    	
-    	
-    	
-    	
-    	
-    	
-     
-     
-     
-     
-     
-     
-     
        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
     	
     	
